08092a5cde3251caf749d95319eea302b38a79c7
JeewHiddala <jeewhiddala@gmail.com>
2021-06-26 17:05:34 +0530

    CREATE: update editor added.

--- backend/src/controllers/editor.controller.js
+++ backend/src/controllers/editor.controller.js
@@ -1,4 +1,5 @@
 const Editor = require('../models/editor.model');       //import editor model
+const mongoose = require("mongoose");
 
 const createEditor = async (req, res) => {       //create a editors to db.
     if (req.body) {
@@ -35,8 +36,20 @@ const getSelectedEditorDetails = async (req, res) => {          //get selected e
     }
 }
 
+const updateSelectedEditor = async (req, res) => {       //update selected editor
+    if (req.params && req.params.id){
+        const {id} = req.params;        // fetching the id of the editor.
+        const editor = req.body;
+
+        if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('No editor With That id');      // validating the editor id
+        const updatedEditor = await Editor.findByIdAndUpdate(id, editor,{new : true});      // find editor and Update editor
+        res.json(updatedEditor);
+    }
+}
+
 module.exports = {
     createEditor,
     getAllEditorsDetails,
-    getSelectedEditorDetails
+    getSelectedEditorDetails,
+    updateSelectedEditor
 };
