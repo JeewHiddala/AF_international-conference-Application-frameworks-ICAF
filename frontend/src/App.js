ba289f34e21ca39be21907e4e11b8c473e323d22
Sahanya Ranaweera <sahanya.ranaweera@gmail.com>
2021-06-28 01:18:45 +0530

    attendee register added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,22 @@
 import './App.css';
 import React from 'react';
-import { BrowserRouter as Router} from 'react-router-dom';    //make routes
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';    //make routes
 import Navbar from './components/navBar/navBar';
+import Home from './components/home/home';
 import Footer from './components/footer/footer';
+import CreateAttendee from './components/attendee/attendeeRegister';
 
 function App(){
     return(
         <div>
             <Router>
                 <Navbar/>
+                <Switch>
+                    <Route exact path='/' component={Home} />
+                    
+                    <Route path='/attendee/create' component={CreateAttendee} />
+                    
+                </Switch>
                 <Footer/>
             </Router>
         </div>
