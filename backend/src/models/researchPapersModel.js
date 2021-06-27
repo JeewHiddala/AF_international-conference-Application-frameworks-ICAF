e501a18fadfa4a15a0eec4a9c27e83906f6f3d79
deshani0000 <it19051826@my.sliit.lk>
2021-06-27 15:33:32 +0530

    research paper model added

---
+++ backend/src/models/researchPapersModel.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');       
+
+const ResearchPaperSchema = new mongoose.Schema({    
+    title: { type: String, required: true, trim: true },
+    authors: { type: String, required: true, trim: true },
+    conferenceName: { type: String, required: true, trim: true },
+    researchArea: { type: String, required: true, trim: true },
+    submissionFile: { type: String, required: true, trim: true },
+    submittedDate: {type: Date},
+    status: { type: String, required: true, trim: true },
+    approvedDate: {type: Date},
+    presenter: [{ type: mongoose.Schema.Types.ObjectId, required: false, ref: 'presenter'}]
+});
+
+const ResearchPaper = mongoose.model('researchPaper', ResearchPaperSchema);      
+module.exports = ResearchPaper;
